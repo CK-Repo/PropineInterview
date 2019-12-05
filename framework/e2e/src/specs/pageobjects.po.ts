b0b5ae19537185a278aeeed13c7e55bd65107886
Chaitanya Krishna <ck.chaitanyakrishna@outlook.com>
2019-12-05 13:33:38 +0530

    Update pageobjects.po.ts

--- framework/e2e/src/specs/pageobjects.po.ts
+++ framework/e2e/src/specs/pageobjects.po.ts
@@ -9,11 +9,6 @@ export class PageObject {
 
     // tslint:disable: quotemark
     readonly HomePagetext = ['xpath', "//h1[contains(.,'Propine Addition Calculator')]"];
-    readonly Screen1loginButton = ['xpath', "//div[@id='screen_393772830']/div/a"];
-    readonly ResetUsername = ['xpath', "//div[@id='screen_393772831']/div/a"];
-    readonly ResetPassword = ['xpath', "//div[@id='screen_393772832']/div/a"];
-    readonly ResetconfirmPass = ['xpath', "//div[@id='screen_393772833']/div/a"];
-    readonly ResetButton = ['xpath', "//div[@id='screen_393772834']/div/a"];
-    readonly StartSystem = ['xpath', "//div[@id='screen_393772835']/div/a"];
+    
 
 }
